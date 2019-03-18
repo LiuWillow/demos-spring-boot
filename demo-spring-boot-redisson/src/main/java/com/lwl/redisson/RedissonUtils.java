afa8e7b428d2e8d00d19206160e19158c8739cfc
LiuWillow <liuwillow9625@163.com>
2019-03-18 19:25:43 +0800

    添加管道操作

--- demo-spring-boot-redisson/src/main/java/com/lwl/redisson/RedissonUtils.java
+++ demo-spring-boot-redisson/src/main/java/com/lwl/redisson/RedissonUtils.java
@@ -1,9 +1,6 @@
 package com.lwl.redisson;
 
-import org.redisson.api.RBucket;
-import org.redisson.api.RLock;
-import org.redisson.api.RMap;
-import org.redisson.api.RedissonClient;
+import org.redisson.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,44 +19,48 @@ public class RedissonUtils {
 
     /**
      * 设置string，不过期
+     *
      * @param key
      * @param value
      */
-    public void set(String key, Object value){
+    public void set(String key, Object value) {
         RBucket<Object> bucket = redissonClient.getBucket(key);
         bucket.set(value);
     }
 
     /**
      * 设置string，设置过期时间，默认毫秒
+     *
      * @param key
      * @param value
      * @param expire
      */
-    public void set(String key, Object value, long expire){
+    public void set(String key, Object value, long expire) {
         RBucket<Object> bucket = redissonClient.getBucket(key);
         bucket.set(value, expire, TimeUnit.MILLISECONDS);
     }
 
-    public Object get(String key){
+    public Object get(String key) {
         RBucket<Object> bucket = redissonClient.getBucket(key);
         return bucket.get();
     }
-    public void delete(String key){
+
+    public void delete(String key) {
         RBucket<Object> bucket = redissonClient.getBucket(key);
         bucket.delete();
     }
 
     /**
      * 用RedLock获取分布式锁，设置客户端等待时间和锁过期时间
+     *
      * @param key
      * @param wait
      * @param expire
      * @return
      */
-    public boolean lock(String key, long wait, long expire){
+    public boolean lock(String key, long wait, long expire) {
         RLock rLock = redissonClient.getLock(key);
-        if (rLock.isLocked()){
+        if (rLock.isLocked()) {
             return false;
         }
         try {
@@ -72,10 +73,11 @@ public class RedissonUtils {
 
     /**
      * 释放锁
+     *
      * @param key
      * @return
      */
-    public boolean releaseLock(String key){
+    public boolean releaseLock(String key) {
         RLock lock = redissonClient.getLock(key);
         lock.unlock();
         return true;
@@ -83,26 +85,39 @@ public class RedissonUtils {
 
     /**
      * 操作hash
+     *
      * @param key
      * @param valueMap
      */
-    public void addMap(String key, Map<Object, Object> valueMap){
+    public void addMap(String key, Map<Object, Object> valueMap) {
         RMap<Object, Object> map = redissonClient.getMap(key);
         map.putAll(valueMap);
     }
 
-    public Object getMap(String mapName, String key){
+    public Object getMap(String mapName, String key) {
         RMap<Object, Object> map = redissonClient.getMap(mapName);
-
         return map.get(key);
     }
 
     /**
      * 复杂度O（n），尽量不用
+     *
      * @param keys
      * @return
      */
-    public Map<String, Object> mGet(String... keys){
+    public Map<String, Object> mGet(String... keys) {
         return redissonClient.getBuckets().get(keys);
+    }
+
+    /**
+     * 管道批量操作
+     * @param keys
+     */
+    public void piplineDelete(String... keys) {
+        RBatch rBatch = redissonClient.createBatch(BatchOptions.defaults());
+        for (String key : keys) {
+            rBatch.getBucket(key).deleteAsync();
+        }
+        rBatch.execute();
     }
 }
